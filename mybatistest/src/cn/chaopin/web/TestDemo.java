80e5a778f3295b1659da9b2097a1f5a083091f66
aleam0722 <cxb444555@outlook.com>
2019-12-11 19:27:35 +0800

    在测试类中调用server层方法尝试进行测试

--- mybatistest/src/cn/chaopin/web/TestDemo.java
+++ mybatistest/src/cn/chaopin/web/TestDemo.java
@@ -1,10 +1,16 @@
 package cn.chaopin.web;
 
+import cn.chaopin.pojo.Student;
+import cn.chaopin.server.StudentServer;
 import org.junit.Test;
 
+import java.util.List;
+
 public class TestDemo {
+    StudentServer studentServer = new StudentServer();
     @Test
     public void queryTest(){
-
+        List<Student> list = studentServer.list();
+        System.out.println(list);
     }
 }
