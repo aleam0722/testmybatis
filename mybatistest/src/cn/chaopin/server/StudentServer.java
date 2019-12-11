c40bb98ee551b57e4778f8cee08bc9d8dfef7b1f
aleam0722 <cxb444555@outlook.com>
2019-12-11 19:20:59 +0800

    在业务层调用mapper（dao层）的方法

--- mybatistest/src/cn/chaopin/server/StudentServer.java
+++ mybatistest/src/cn/chaopin/server/StudentServer.java
@@ -1,4 +1,16 @@
 package cn.chaopin.server;
 
+import cn.chaopin.mapper.StudentMapper;
+import cn.chaopin.pojo.Student;
+
+import java.util.List;
+
 public class StudentServer {
+//   将mapper接口定义为StudentServer的属性
+    private StudentMapper studentMapper;
+//    在业务层调用mapper（dao层）的方法
+    public List<Student> list() {
+        return studentMapper.list();
+    }
+
 }
