442ab3e115b07c69370aafa5126edef59d32824f
aleam0722 <cxb444555@outlook.com>
2019-12-12 10:07:20 +0800

    xxxxx

--- mybatistest/src/cn/chaopin/server/StudentServer.java
+++ mybatistest/src/cn/chaopin/server/StudentServer.java
@@ -8,7 +8,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 public class StudentServer {
@@ -16,16 +15,15 @@ public class StudentServer {
     private StudentMapper studentMapper;
 //    在业务层调用mapper（dao层）的方法
     public List<Student> list() throws IOException {
+        System.out.println("studentMapper = " + studentMapper);
         //      mybatis的模板代码
-        String resource = "";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(StudentServer.class.getClassLoader().getResourceAsStream("mybatis-3-cfg.xml"));
+        System.out.println("sqlSessionFactory = " + sqlSessionFactory);
 //       得到sqlsession的工厂对象之后需要将通过openSession方法获得sqlSession对象进行操作
         SqlSession sqlSession = sqlSessionFactory.openSession();
 //        打开session后，通过getMapper方法获取代理对象
         studentMapper = sqlSession.getMapper(StudentMapper.class);
-         return studentMapper.list();
+        return studentMapper.list();
     }
 
 }
--- mybatistest/src/cn/chaopin/web/TestDemo.java
+++ mybatistest/src/cn/chaopin/web/TestDemo.java
@@ -4,12 +4,14 @@ import cn.chaopin.pojo.Student;
 import cn.chaopin.server.StudentServer;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 public class TestDemo {
     StudentServer studentServer = new StudentServer();
     @Test
-    public void queryTest(){
+    public void queryTest() throws IOException {
+
         List<Student> list = studentServer.list();
         System.out.println(list);
     }
